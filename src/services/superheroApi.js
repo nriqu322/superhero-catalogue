07f5c3e626f6ed3d0dcbd356f29954a2ee922987
Luis Saavedra <nriqu322@gmail.com>
2020-09-18 10:11:43 -0600

    delete commented code

--- src/services/superheroApi.js
+++ src/services/superheroApi.js
@@ -1,18 +1,8 @@
-// import axios from 'axios';
-
 // const cors = 'https://cors-anywhere.herokuapp.com/';
 const api = {
   baseUrl: 'https://superheroapi.com/api/',
   token: '10158849047488054',
 };
-
-// const url = `${api.baseUrl}${api.token}`;
-
-// const getSuperhero = async (name = '') => {
-//   axios
-//     .get(`${cors}${url}/search/${name}`)
-//     .then(response => response.data);
-// };
 
 async function getSuperhero(name) {
   try {
@@ -25,16 +15,5 @@ async function getSuperhero(name) {
     };
   }
 }
-
-// const getSuperhero = name => new Promise((resolve, reject) => {
-//   axios
-//     .get(
-//       `${cors}${api.baseUrl}${api.token}/search/${name}`,
-//     )
-//     .then(data => {
-//       resolve(data.data.results);
-//       reject(new Error('something bad happened'));
-//     });
-// });
 
 export default getSuperhero;
