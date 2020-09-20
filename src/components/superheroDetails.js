10b3f53fe0ed996201f69fda790e5728bcbce052
Luis Saavedra <nriqu322@gmail.com>
2020-09-20 01:37:14 -0600

    style details page

--- src/components/superheroDetails.js
+++ src/components/superheroDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const SuperheroDetails = props => {
   const { match } = props;
@@ -12,9 +13,13 @@ const SuperheroDetails = props => {
 
   return (
     <div className="superhero-details">
-      <h1>Go Back</h1>
+      <div className="container-back">
+        <Link to="/">
+          <h1 className="back">Go Back</h1>
+        </Link>
+      </div>
       <div className="container-bio">
-        <h2>Biography</h2>
+        <h2 className="stats-title">Biography</h2>
         <div className="hero-bio">
           <img src={currentHero.image.url} alt="hero" />
           <div className="hero-bio-text">
@@ -41,58 +46,59 @@ const SuperheroDetails = props => {
         </div>
       </div>
       <hr />
+      <div className="details">
+        <div className="container-appearance">
+          <h2 className="stats-title">Appearance</h2>
+          <div className="hero-appearance">
+            <h4>Gender</h4>
+            <p>{currentHero.appearance.gender}</p>
+            <h4>Eye Color</h4>
+            <p>{currentHero.appearance['eye-color']}</p>
+            <h4>Hair Color</h4>
+            <p>{currentHero.appearance['hair-color']}</p>
+            <h4>Race</h4>
+            <p>{currentHero.appearance.race}</p>
+            <h4>Height</h4>
+            <p>{`${currentHero.appearance.height[0]} / ${currentHero.appearance.height[1]}`}</p>
+            <h4>Weight</h4>
+            <p>{`${currentHero.appearance.weight[0]} / ${currentHero.appearance.weight[1]}`}</p>
+          </div>
+        </div>
+        <div className="container-power">
+          <h2 className="stats-title">Power Stats</h2>
+          <div className="hero-power">
+            <h4>Combat</h4>
+            <p>{currentHero.powerstats.combat}</p>
+            <h4>Durability</h4>
+            <p>{currentHero.powerstats.durability}</p>
+            <h4>Intelligence</h4>
+            <p>{currentHero.powerstats.intelligence}</p>
+            <h4>Power</h4>
+            <p>{currentHero.powerstats.power}</p>
+            <h4>Speed</h4>
+            <p>{currentHero.powerstats.speed}</p>
+            <h4>Strength</h4>
+            <p>{currentHero.powerstats.strength}</p>
+          </div>
+        </div>
 
-      <div className="container-appearance">
-        <h2>Appearance</h2>
-        <div className="hero-appearance">
-          <h4>Gender</h4>
-          <p>{currentHero.appearance.gender}</p>
-          <h4>Eye Color</h4>
-          <p>{currentHero.appearance['eye-color']}</p>
-          <h4>Hair Color</h4>
-          <p>{currentHero.appearance['hair-color']}</p>
-          <h4>Race</h4>
-          <p>{currentHero.appearance.race}</p>
-          <h4>Height</h4>
-          <p>{`${currentHero.appearance.height[0]} / ${currentHero.appearance.height[1]}`}</p>
-          <h4>Weight</h4>
-          <p>{`${currentHero.appearance.weight[0]} / ${currentHero.appearance.weight[1]}`}</p>
+        <div className="container-connections">
+          <h2 className="stats-title">Connections</h2>
+          <div className="hero-connections">
+            <h4>Group Affiliation</h4>
+            <p>{currentHero.connections['group-affiliation']}</p>
+            <h4>Relatives</h4>
+            <p>{currentHero.connections.relatives}</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <h2>Power Stats</h2>
-        <div className="hero-power">
-          <h4>Combat</h4>
-          <p>{currentHero.powerstats.combat}</p>
-          <h4>Durability</h4>
-          <p>{currentHero.powerstats.durability}</p>
-          <h4>Intelligence</h4>
-          <p>{currentHero.powerstats.intelligence}</p>
-          <h4>Power</h4>
-          <p>{currentHero.powerstats.power}</p>
-          <h4>Speed</h4>
-          <p>{currentHero.powerstats.speed}</p>
-          <h4>Strength</h4>
-          <p>{currentHero.powerstats.strength}</p>
-        </div>
-      </div>
-
-      <div className="container-connections">
-        <h2>Connections</h2>
-        <div className="hero-connections">
-          <h4>Group Affiliation</h4>
-          <p>{currentHero.connections['group-affiliation']}</p>
-          <h4>Relatives</h4>
-          <p>{currentHero.connections.relatives}</p>
-        </div>
-      </div>
-      <div className="container-work">
-        <h2>Work</h2>
-        <div className="hero-work">
-          <h4>Base</h4>
-          <p>{currentHero.work.base}</p>
-          <h4>Occupation</h4>
-          <p>{currentHero.work.occupation}</p>
+        <div className="container-work">
+          <h2 className="stats-title">Work</h2>
+          <div className="hero-work">
+            <h4>Base</h4>
+            <p>{currentHero.work.base}</p>
+            <h4>Occupation</h4>
+            <p>{currentHero.work.occupation}</p>
+          </div>
         </div>
       </div>
     </div>
