89230b41227a0b05f3fdd11513296fb21a6b94f2
Luis Saavedra <nriqu322@gmail.com>
2020-09-22 12:57:30 -0600

    move @testing to dev dependencies

---
+++ src/_tests_/App.test.js
@@ -0,0 +1,10 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../components/App';
+import Header from '../components/header';
+
+it('renders app', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+});
